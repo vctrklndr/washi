760b2bf7bb4e9614b162282b2ae80b16e86e1559
vctrklndr <victor@nasa.se>
2018-11-26 23:16:36 +0100

    More .less files and a navigation bar

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,24 @@
 <template>
-  <div id="hello">
-    <img src="src/Assets/Images/washi-logo.svg" width="200px">
-    <h1>{{ message }}</h1>
-    <span class="Button">Hej</span>
+  <div>
+    <header class="Header">
+      <div class="Page-container">
+        <div class="Header-container">
+          <img class="Header-logo" src="src/Assets/Images/washi-logo.svg" width="200px">
+          <nav class="PageNav">
+            <ul class="PageNav-list">
+              <li class="PageNav-Link">Boka tvättid</li>
+              <li class="PageNav-Link">Regler</li>
+              <li class="PageNav-Link">Tvättråd</li>
+              <li class="PageNav-Link">Logga ut</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <div class="Page-container">
+      <h1>{{ message }}</h1>
+      <span class="Button">Hej</span>
+    </div>
   </div>
 </template>
 
