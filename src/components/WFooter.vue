826995cf8932c964baaaf36fb495a90a3100858f
vctrklndr <victor@nasa.se>
2018-12-06 22:41:32 +0100

    Added footer

---
+++ src/components/WFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="Footer">
+    <div class="Page-container">
+      <div class="Footer-container">
+        <section class="Section">
+          <p class="u-marginAz">Hej</p>
+        </section>
+      </div>
+    </div>
+  </footer>
+</template>
