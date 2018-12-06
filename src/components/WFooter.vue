4368cb30408e7308448b410a91ceeda5500ddd29
vctrklndr <victor@nasa.se>
2018-12-07 00:06:43 +0100

    Add larger logo to footer

--- src/components/WFooter.vue
+++ src/components/WFooter.vue
@@ -3,9 +3,9 @@
     <div class="Page-container">
       <div class="Footer-container">
         <section class="Section u-textCenter">
-          <img class="Header-logo" src="src/Assets/Images/washi-logo.svg" alt="Washi logo">
+          <img class="Footer-logo" src="src/Assets/Images/washi-logo.svg" alt="Washi logo">
           <ul class="List">
-            <li class="u-textXSmall">Hello from footer</li>
+            <li class="u-textXSmall"><small style="letter-spacing: .05rem;">hello from footer</small></li>
           </ul>
         </section>
       </div>
