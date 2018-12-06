5c7205e1109597d2fa2baf66bc585c856873c4ce
vctrklndr <victor@nasa.se>
2018-12-06 23:53:51 +0100

    Added u-textCenter to footer section

--- src/components/WFooter.vue
+++ src/components/WFooter.vue
@@ -2,10 +2,10 @@
   <footer class="Footer">
     <div class="Page-container">
       <div class="Footer-container">
-        <section class="Section">
+        <section class="Section u-textCenter">
           <img class="Header-logo" src="src/Assets/Images/washi-logo.svg" alt="Washi logo">
-          <ul class="List u-textCenter">
-            <li class="u-textXSmall">Hej</li>
+          <ul class="List">
+            <li class="u-textXSmall">Hello from footer</li>
           </ul>
         </section>
       </div>
