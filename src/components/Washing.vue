22b559338095d3919e43d9aadc02762941625044
vctrklndr <victor@nasa.se>
2018-12-10 14:55:58 +0100

    Created a icons folder in images

--- src/components/Washing.vue
+++ src/components/Washing.vue
@@ -5,92 +5,92 @@
       <p>Här förklarar vi de vanligaste tvättsymbolerna.</p>
       <div class="Grid u-textCenter u-marginTlg">
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/no_waterwash.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/no_waterwash.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Ej vattentvätt</h2>
           <p class="u-textXSmall u-marginTz">Ska inte tvättas i vatten</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/hand_wash.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/hand_wash.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Handtvätt</h2>
           <p class="u-textXSmall u-marginTz">Tål ej maskintvätt</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/machine_wash_30.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/machine_wash_30.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Maskintvätt</h2>
           <p class="u-textXSmall u-marginTz">Tvättas i max 30°C</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/machine_wash_40.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/machine_wash_40.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Maskintvätt</h2>
           <p class="u-textXSmall u-marginTz">Tvättas i max 40°C </p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/machine_wash_60.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/machine_wash_60.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Maskintvätt</h2>
           <p class="u-textXSmall u-marginTz">Tvättas i max 60°C</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/no_dry_clean.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/no_dry_clean.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Ej kemtvätt</h2>
           <p class="u-textXSmall u-marginTz">Kemtvättas ej</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/dry_clean.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/dry_clean.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Kemtvätt</h2>
           <p class="u-textXSmall u-marginTz">Får kemtvättas</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/dry_clean_p.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/dry_clean_p.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Kemtvätt</h2>
           <p class="u-textXSmall u-marginTz">Endast kemtvätt med perkloretylen</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/no_tumble_dry.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/no_tumble_dry.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Torktumlas ej</h2>
           <p class="u-textXSmall u-marginTz">Ska inte torktumlas</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/tumble_dry.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/tumble_dry.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Torktumlas</h2>
           <p class="u-textXSmall u-marginTz">Kan torktumlas</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/tumble_dry_low_heat.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/tumble_dry_low_heat.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Torktumlas</h2>
           <p class="u-textXSmall u-marginTz">Torktumlas på låg värme</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/tumble_dry_high_heat.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/tumble_dry_high_heat.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Torktumlas</h2>
           <p class="u-textXSmall u-marginTz">Torktumlas på hög värme</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/no_bleach.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/no_bleach.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Ej blekmedel</h2>
           <p class="u-textXSmall u-marginTz">Blekmedel får ej användas</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/bleach.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/bleach.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Blekmedel</h2>
           <p class="u-textXSmall u-marginTz">Blekmedel får användas</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/do_not_iron.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/do_not_iron.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Får ej strykas</h2>
           <p class="u-textXSmall u-marginTz">–</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/iron_low.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/iron_low.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Strykes</h2>
           <p class="u-textXSmall u-marginTz">Strykes max 110°C</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/iron_medium.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/iron_medium.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Strykes</h2>
           <p class="u-textXSmall u-marginTz">Strykes max 150°C</p>
         </div>
         <div class="Grid-cell u-size1of2 u-sm-size1of3 u-md-size1of4 u-lg-size1of6 u-marginVmd">
-          <img class="Icon-washing" src="src/Assets/Images/iron_high.svg">
+          <img class="Icon-washing" src="src/Assets/Images/Icons/iron_high.svg">
           <h2 class="Heading Heading--h2 u-marginBz">Strykes</h2>
           <p class="u-textXSmall u-marginTz">Strykes max 200°C</p>
         </div>
