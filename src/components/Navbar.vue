d03f823459d7637e7236d31dc981410f9de43edf
vctrklndr <victor@nasa.se>
2018-12-16 16:25:41 +0100

    Added class to #app on hamburger click, so the overflow would be hidden

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,7 @@
           >
         </router-link>
         <nav class="PageNav">
-          <div class="Hamburger" :class="{'Hamburger--open': isActive}" @click="toggleMenu">
+          <div class="Hamburger" :class="{'Hamburger--open': isActive}" @click="toggleMenu(); disableScroll();">
             <span></span>
             <span></span>
             <span></span>
@@ -41,22 +41,33 @@
 </template>
 
 <script>
-
 export default {
-  name: 'navbar',
+  name: "navbar",
   data() {
     return {
       isActive: false
-    }
+    };
   },
   methods: {
-    toggleMenu: function () {
+    toggleMenu: function() {
       this.isActive = !this.isActive;
     },
-    closeMenu: function () {
+    disableScroll: function() {
+      const app = document.getElementById("app");
+      if (app.classList.contains('disable-scroll')){
+        app.classList.remove('disable-scroll');     
+      } else {
+        app.classList.add('disable-scroll');   
+      }
+    },
+    closeMenu: function() {
       this.isActive = false;
+      const app = document.getElementById("app");
+      app.classList.remove("disable-scroll");
+      window.scrollTo({
+        top: 0
+      });
     }
   }
-}
-
+};
 </script>
