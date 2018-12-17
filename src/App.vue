d41595904506572a7a759cb7c1efe803f40a1b49
vctrklndr <victor@nasa.se>
2018-12-17 13:45:28 +0100

    Added a login router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <navbar />
+      <div class="Page-container">
+        <main class="Page-main">
+            <router-view></router-view>
+        </main>
+      </div>
+    <w-footer/>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+import WFooter from "./components/WFooter.vue";
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    Navbar,
+    WFooter
+  }
 };
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="Login-container">
-asd
+  <div class="Login-container u-sizeFull">
+    <img class="Login-logo u-marginVlg" src="src/Assets/Images/washi-logo-brandcolor.svg" alt="Logo" />
+    <label class="Input-label u-textXSmall" for="username">Användarnamn:</label>
+    <input class="Input" id="username" type="text" placeholder="Användarnamn" />
+    <label class="Input-label u-marginTsm u-textXSmall" for="username">Lösenord:</label>
+    <input class="Input" id="password" type="password" placeholder="Lösenord" />
+    <router-link class="Button u-marginTlg" to="/home">Logga in</router-link>
+    <router-link class="Link u-textXSmall" to="/">Admin</router-link>
   </div>
 </template>
 
