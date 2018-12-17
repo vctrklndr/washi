a41cf2ce57e332b4021dd25ff863247823f7a528
vctrklndr <victor@nasa.se>
2018-12-17 16:16:35 +0100

    Small issues fixes

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -16,10 +16,7 @@
             <time
               class="Calendar-selectedDate"
             >{{month.charAt(0).toUpperCase() + month.slice(1) + ' - ' + year}}</time>
-            <span 
-              @click="addMonth"
-              class="Calendar-controls"
-            >Nästa &gt;</span>
+            <span @click="addMonth" class="Calendar-controls">Nästa &gt;</span>
           </div>
           <div class="Calendar u-paddingBz">
             <button
@@ -93,179 +90,192 @@
       </div>
     </div>
   </section>
-  
 </template>
 
 <script>
-
-
 import moment from "moment";
-import 'moment/locale/sv';
-moment.updateLocale('sv', {
+import "moment/locale/sv";
+moment.updateLocale("sv", {
   week: {
-    dow: 1,
-  },
-})
+    dow: 1
+  }
+});
 export default {
-  
   data() {
     return {
       today: moment(),
       dateContext: moment(),
-      displayDate: moment().format('dddd' +' D ' + 'MMMM'),
-      days: ['M', 'T', 'O', 'T', 'F', 'L', 'S'],
-      times: ['06.00 – 09.00', '09.00 – 12.00', '12.00 – 15.00', '15.00 – 18.00', '18.00 – 21.00'],
-      selectedDate: '',
-      selectedTime: '',
-      errorMessage : "",
-      successMessage : "",
+      displayDate: moment().format("dddd" + " D " + "MMMM"),
+      days: ["M", "T", "O", "T", "F", "L", "S"],
+      times: [
+        "06.00 – 09.00",
+        "09.00 – 12.00",
+        "12.00 – 15.00",
+        "15.00 – 18.00",
+        "18.00 – 21.00"
+      ],
+      selectedDate: "",
+      selectedTime: "",
+      errorMessage: "",
+      successMessage: "",
       users: [],
-      newBooking: {selectedDate: '' , selectedTime: ''},
-      newUser: {selectedTime: "mumma", apartmentNumber: "gumma", password: "fumma"},
-      clickedUser: {},
-    }
+      newBooking: { selectedDate: "", selectedTime: "" },
+      newUser: {
+        selectedTime: "mumma",
+        apartmentNumber: "gumma",
+        password: "fumma"
+      },
+      clickedUser: {}
+    };
   },
   computed: {
-    year: function () {
-      return this.dateContext.format('Y');
+    year: function() {
+      return this.dateContext.format("Y");
     },
-    month: function () {
-      return this.dateContext.format('MMMM');
+    month: function() {
+      return this.dateContext.format("MMMM");
     },
     dayOfMonth: function() {
-      return this.dateContext.format('D');
+      return this.dateContext.format("D");
     },
-    daysInMonth: function () {
-      return this.dateContext.daysInMonth('D');
+    daysInMonth: function() {
+      return this.dateContext.daysInMonth("D");
     },
-    currentDate: function () {
-      return this.dateContext.get('D');
+    currentDate: function() {
+      return this.dateContext.get("D");
     },
-    firstDayOfMonth: function () {
-      const firstDay = moment(this.dateContext).subtract((this.currentDate - 1), 'days');
+    firstDayOfMonth: function() {
+      const firstDay = moment(this.dateContext).subtract(
+        this.currentDate - 1,
+        "days"
+      );
       return firstDay.weekday();
     },
-    initialDate: function () {
-      return this.today.get('D');
+    initialDate: function() {
+      return this.today.get("D");
     },
-    initialMonth: function () {
-      return this.today.format('MMMM');
+    initialMonth: function() {
+      return this.today.format("MMMM");
     },
-    initialYear: function () {
-      return this.today.format('Y');
+    initialYear: function() {
+      return this.today.format("Y");
     }
   },
-  mounted: function () {
-		console.log("Hi KK");
-		this.getAllUsers();
-	},
+  mounted: function() {
+    console.log("Hi KK");
+    this.getAllUsers();
+  },
   methods: {
-    
-    addMonth: function () {
-      this.dateContext = moment(this.dateContext).add(1, 'month');
+    addMonth: function() {
+      this.dateContext = moment(this.dateContext).add(1, "month");
     },
-    subtractMonth: function () {
-      this.dateContext = moment(this.dateContext).subtract(1, 'month');
+    subtractMonth: function() {
+      this.dateContext = moment(this.dateContext).subtract(1, "month");
     },
-    selectDate: function (date){
-      const month = this.dateContext.format('MMMM');
-      const year = this.dateContext.format('YYYY');
-      const day = moment(year + month + date).format('YYYY-MM-DD');
+    selectDate: function(date) {
+      const month = this.dateContext.format("MMMM");
+      const year = this.dateContext.format("YYYY");
+      const day = moment(year + month + date).format("YYYY-MM-DD");
 
       this.selectedDate = day;
-      this.selectedTime = '';
-      this.displayDate = moment(year + month + date).format('dddd' +' D ' + 'MMMM');
+      this.selectedTime = "";
+      this.displayDate = moment(year + month + date).format(
+        "dddd" + " D " + "MMMM"
+      );
       console.log(this.selectedDate);
-      console.log(this.isActive);
       //this.newBooking = {selectedDate: day};
     },
-    selectTime: function(time){      
+    selectTime: function(time) {
       this.selectedTime = "tid" + time;
-      this.newBooking = {selectedDate: this.selectedDate, selectedTime: "tid" + time};
+      this.newBooking = {
+        selectedDate: this.selectedDate,
+        selectedTime: "tid" + time
+      };
 
       console.log(this.selectedTime);
-    },getAllUsers: function(){
-			axios.get("http://mikahl.se/VuePHP/api.php?action=read")
-			.then(function(response){
-        console.log(response);
-				if (response.data.error) {
-          this.errorMessage = response.data.message;
-          console.log("testing")
-				}else{
-          this.users = response.data.users;
-          console.log(this.users)
-				}
-			});
-		},
-		saveUser:function(){
-
-			var formData = this.toFormData(this.newBooking);
-			axios.post("http://mikahl.se/VuePHP/api.php?action=create", formData)
-				.then(function(response){
+    },
+    getAllUsers: function() {
+      axios
+        .get("http://mikahl.se/VuePHP/api.php?action=read")
+        .then(function(response) {
+          console.log(response);
+          if (response.data.error) {
+            this.errorMessage = response.data.message;
+            console.log("testing");
+          } else {
+            this.users = response.data.users;
+            console.log(this.users);
+          }
+        });
+    },
+    saveUser: function() {
+      var formData = this.toFormData(this.newBooking);
+      axios
+        .post("http://mikahl.se/VuePHP/api.php?action=create", formData)
+        .then(function(response) {
           console.log(response);
           //this.newUser = {userId: "", apartmentNumber: "", password: ""};
-           //selectedTime 
-					if (response.data.error) {
-						this.errorMessage = response.data.message;
-					}else{
-						this.successMessage = response.data.message;
-						this.getAllUsers();
-					}
-				});
-			},
-			updateUser:function(){
+          //selectedTime
+          if (response.data.error) {
+            this.errorMessage = response.data.message;
+          } else {
+            this.successMessage = response.data.message;
+            this.getAllUsers();
+          }
+        });
+    },
+    updateUser: function() {
+      var formData = app.toFormData(app.clickedUser);
+      axios
+        .post("http://localhost:8888/VuePHP/api.php?action=update", formData)
+        .then(function(response) {
+          console.log(response);
+          app.clickedUser = {};
+          if (response.data.error) {
+            app.errorMessage = response.data.message;
+          } else {
+            app.successMessage = response.data.message;
+            app.getAllUsers();
+          }
+        });
+    },
+    deleteUser: function() {
+      var formData = app.toFormData(app.clickedUser);
+      axios
+        .post("http://localhost:8888/VuePHP/api.php?action=delete", formData)
+        .then(function(response) {
+          console.log(response);
+          app.clickedUser = {};
+          if (response.data.error) {
+            app.errorMessage = response.data.message;
+          } else {
+            app.successMessage = response.data.message;
+            app.getAllUsers();
+          }
+        });
+    },
+    selectUser(user) {
+      app.clickedUser = user;
+    },
 
-			var formData = app.toFormData(app.clickedUser);
-			axios.post("http://localhost:8888/VuePHP/api.php?action=update", formData)
-				.then(function(response){
-					console.log(response);
-					app.clickedUser = {};
-					if (response.data.error) {
-						app.errorMessage = response.data.message;
-					}else{
-						app.successMessage = response.data.message;
-						app.getAllUsers();
-					}
-				});
-			},
-			deleteUser:function(){
-
-			var formData = app.toFormData(app.clickedUser);
-			axios.post("http://localhost:8888/VuePHP/api.php?action=delete", formData)
-				.then(function(response){
-					console.log(response);
-					app.clickedUser = {};
-					if (response.data.error) {
-						app.errorMessage = response.data.message;
-					}else{
-						app.successMessage = response.data.message;
-						app.getAllUsers();
-					}
-				});
-			},
-			selectUser(user){
-				app.clickedUser = user;
-			},
-
-			toFormData: function(obj){
-        console.log(obj)
-				var form_data = new FormData();
-			      for ( var key in obj ) {
-			          form_data.append(key, obj[key]);
-			      }
-			      return form_data;
-			},
-			clearMessage: function(){
-				app.errorMessage = "";
-				app.successMessage = "";
-			},
-    bookTime: function(date, time){
-      if(date !== "" && time !== "") {
-        console.log(date,time)
+    toFormData: function(obj) {
+      console.log(obj);
+      var form_data = new FormData();
+      for (var key in obj) {
+        form_data.append(key, obj[key]);
+      }
+      return form_data;
+    },
+    clearMessage: function() {
+      app.errorMessage = "";
+      app.successMessage = "";
+    },
+    bookTime: function(date, time) {
+      if (date !== "" && time !== "") {
+        console.log(date, time);
       }
       return null;
     }
   }
-}
-
+};
 </script>
