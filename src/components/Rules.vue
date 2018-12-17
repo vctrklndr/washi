f418061c199ef6aaa8b748d7caacea8c01a7aef9
vctrklndr <victor@nasa.se>
2018-12-17 15:46:41 +0100

    Added more content to Home.vue and Rules.vue

--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -2,9 +2,47 @@
   <section class="Section Section--contain">
     <div class="Page-container">
       <h1 class="Heading Heading--h1 Heading-line">Regler</h1>
-      <h4 class="Heading Heading--h2">Regler att följa:</h4>
-      <ul class="List">
-        <li>Den som inte tar bort luddet ska dö</li>
+      <p class="u-md-size1of2 u-textSpacing">
+        Tänk på att tvättstugorna är ett gemensamt utrymme där alla skall trivas
+        och kunna utföra sitt tvättande på ett bra sätt. Lämna tvättstugan på samma
+        sätt som Du önskar att den skall vara när Du kommer för att tvätta.
+      </p>
+      <ul class="u-md-size1of2 u-marginTxlg">
+        <li class="List-item u-textLarge ">Den som inte tar bort luddet ska dö</li>
+        <li class="List-item">Du kan tvätta mellan klockan
+          <span class="u-textHighlight u-textLarge ">07.00</span> och
+          <span class="u-textHighlight u-textLarge ">22.00</span> (efter kl. 22.30 kommer du inte in)
+        </li>
+        <li class="List-item u-textLarge ">
+          Du kommer in i tvättstugan upp till 30 min efter avslutat pass - för att ta hand
+          om tvätten som har torkat.
+          <span class="u-textHighlight">Tänk på att inte starta en maskin precis innan passets slut!</span>
+        </li>
+        <li class="List-item u-textLarge ">
+          Den som har bokat ett tvättpass och inte påbörjat tvättningen senast 30 minuter in på
+          tvättpasset har förbrukat sin rätt till tvättpasset. Annan bostadsrättsinnehavare kan då
+          boka och utnyttja tvättpasset.
+        </li>
+        <li class="List-item u-textLarge ">
+          Den som har använt tvättstugan ska se till att lokalerna och all utrustning rengörs
+          noga. Golvet ska sopas och våttorkas.
+        </li>
+        <li class="List-item u-textLarge ">
+          <span class="u-textHighlight">Filtret i torktumlaren ska rensas</span> (brandrisk om detta inte sker)
+        </li>
+        <li class="List-item u-textLarge ">
+          Var rädd om maskinerna. Om det uppstår skador på tvättutrustningen som du orsakat
+          genom oaktsamhet kan Du bli ersättningsskyldig.
+        </li>
+        <li class="List-item u-textLarge ">Det är absolut förbjudet att färga kläder i tvättmaskinerna.</li>
+        <li class="List-item u-textLarge ">Mattor och annan grovtvätt får endast tvättas i
+          <span class="u-textHighlight">grovtvättmaskinen</span>.
+        </li>
+        <li class="List-item u-textLarge ">
+          <span
+            class="u-textHighlight"
+          >Mattor med gummerad undersidan får inte tvättas i någon maskin</span>.
+        </li>
       </ul>
     </div>
   </section>
@@ -12,6 +50,6 @@
 
 <script>
 export default {
-  name: 'help'
-}
+  name: "help"
+};
 </script>
