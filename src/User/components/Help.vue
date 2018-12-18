be04933ce7cb5e9b0b523e7cf5a72a847b0962cb
vctrklndr <victor@nasa.se>
2018-12-18 17:11:00 +0100

    Updated Help and Rules

--- src/User/components/Help.vue
+++ src/User/components/Help.vue
@@ -2,16 +2,16 @@
   <section class="Section Section--contain">
     <div class="Page-container">
       <h1 class="Heading Heading--h1 Heading-line">Hjälp</h1>
-      <p class="u-md-size1of2 u-textSpacing">För att boka en tvättid följer du följande anvisningar:</p>
-      <ul class="u-md-size1of2 u-marginTlg">
-        <li class="List-item u-textLarge">Börja med att gå in på
-          <span class="u-textHighlight u-textLarge">Boka tvättid</span>.
+      <p class="u-md-size2of3 u-textLarge">För att boka en tvättid följer du följande anvisningar:</p>
+      <ul class="u-md-size2of3 u-marginTlg">
+        <li class="List-item u-textSpacing">Börja med att gå in på
+          <span class="u-textHighlight">Boka tvättid</span>.
         </li>
         <li
-          class="List-item u-textLarge"
+          class="List-item u-textSpacing"
         >Därefter klickar du dig fram till önskat datum för att se tillgängliga tvättider.</li>
-        <li class="List-item u-textLarge">Välj önskad tid och tryck
-          <span class="u-textHighlight u-textLarge">"Boka tid"</span>.
+        <li class="List-item u-textSpacing">Välj önskad tid och tryck
+          <span class="u-textHighlight">"Boka tid"</span>.
         </li>
       </ul>
     </div>
