38a1a0077141b066461bb6effd3aefd3b091b63d
vctrklndr <victor@nasa.se>
2018-12-19 18:43:34 +0100

    Updated some stuff

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,12 @@
         <section class="Section u-textCenter">
           <img class="Footer-logo" src="src/Assets/Images/washi-logo.svg" alt="Washi logo">
           <ul class="List">
-            <li class="u-textXSmall"><small style="letter-spacing: .05rem;">hello from footer</small></li>
+            <li class="u-textWeightBold">Kungshem Fastigheter</li>
+            <li class="u-textXSmall">
+              Kungsgatan 5
+            </li>
+            <li class="u-textXSmall">611 31 Nyköping</li>
+            <li class="List-item">0155-20 51 30</li>
           </ul>
         </section>
       </div>
