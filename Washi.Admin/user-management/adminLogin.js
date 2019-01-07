d7d2924933ee2bdf949c700c845e56aeb890b156
vctrklndr <victor@nasa.se>
2019-01-07 20:16:21 +0100

    var to const

--- Washi.Admin/user-management/adminLogin.js
+++ Washi.Admin/user-management/adminLogin.js
@@ -1,16 +1,15 @@
-
-var authUsername = 'admin'
-var authPassword = 'pass'
-var loginButton = document.getElementById('adminLoginButton')
-var loginErrorMessage = document.getElementById('loginErrorMessage')
-loginButton.addEventListener('click', function () {
-  var username = document.getElementById('admin-username').value
-  var password = document.getElementById('admin-password').value
+const authUsername = "admin";
+const authPassword = "pass";
+const loginButton = document.getElementById("adminLoginButton");
+const loginErrorMessage = document.getElementById("loginErrorMessage");
+loginButton.addEventListener("click", function() {
+  const username = document.getElementById("admin-username").value;
+  const password = document.getElementById("admin-password").value;
   if (username === authUsername && password === authPassword) {
-    console.log('Valid!');
-    window.location.href = 'login.html'
+    console.log("Valid!");
+    window.location.href = "login.html";
   } else {
-    console.log('Error!');
-    loginErrorMessage.className = 'showErrorMessage'
+    console.log("Error!");
+    loginErrorMessage.className = "showErrorMessage";
   }
-})
+});
