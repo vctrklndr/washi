414c64ef90567b290254a7b99780f68f1c7ca16b
vctrklndr <victor@nasa.se>
2019-01-08 15:41:39 +0100

    Removed stuff

--- src/components/BookingInformation.vue
+++ src/components/BookingInformation.vue
@@ -3,13 +3,9 @@
     <h2 class="Heading Heading--h2 Calendar-header u-marginTz">Boka tvättid</h2>
     <p class="u-textLarge">För att boka en tvättid följer du följande anvisningar:</p>
     <ul class="u-marginTlg">
-      <li class="List-item u-textSpacing">
-        Börja med att gå in på
-        <span class="u-textHighlight">Boka tvättid</span>.
-      </li>
       <li
         class="List-item u-textSpacing"
-      >Därefter klickar du dig fram till önskat datum för att se tillgängliga tvättider.</li>
+      >Börja med att klicka dig fram till önskat datum för att se tillgängliga tvättider.</li>
       <li class="List-item u-textSpacing">
         Välj önskad tid och tryck
         <span class="u-textHighlight">"Boka tid"</span>.
