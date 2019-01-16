c654575ba08af8bc4947bd9d9707ebf644633c25
vctrklndr <victor@nasa.se>
2019-01-16 11:47:02 +0100

    Changed password for admin

--- Washi.Admin/src/partials/variables.php
+++ Washi.Admin/src/partials/variables.php
@@ -1,6 +1,6 @@
 <?php
 $username = 'admin';
-$password = 'mumma';
+$password = 'p4ssw0rd';
 
 $random1 = 'LURVjF5H6!e08wf!7pQZrlcK2HuWSb2Wdo7WPu4@i@%c&Ktist';
 $random2 = 'FQAvwhoFGF1M!Cs*fG!ZWxV*o%&hrKFCmx*T8MTsnOwdkJzED3';
